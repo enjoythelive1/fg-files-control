dde67c5e59918873a0e71fe4b01d57484e5f3bb9
enjoythelive1 <yonosoy32>
2016-09-03 19:23:14 +0000

    Exposed uploader service

--- src/files-control.module.ts
+++ src/files-control.module.ts
@@ -1,9 +1,11 @@
 import {FilesControl} from "./files-control.component";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
+import {UploadService} from "./upload.service";
 @NgModule({
     imports: [FormsModule],
     declarations: [FilesControl],
+    providers: [UploadService],
     exports: [FilesControl]
 })
 export class FilesControlModule {
